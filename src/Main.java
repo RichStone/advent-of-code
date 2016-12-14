eebb314bcdd2fed7a7222dc1616750cb8b3f78da
Rich Stone <richard.had@hotmail.de>
2016-12-14 13:42:51 +0100

    second door

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
-import advent_door_01.Coordinator;
+import java.util.Random;
 
 public class Main 
 {
 	public static void main (String [] args) throws Exception {
-		Coordinator.calculateWay();
+
 	}
 }
---
+++ src/advent_door_02/NumPadGraph.java
@@ -0,0 +1,5 @@
+package advent_door_02;
+
+public class NumPadGraph {
+	private final int VERTS = 9;
+}
