0f3131445d0417e1e2edb4140971183c1ce31037
Rich Stone <richard.had@hotmail.de>
2016-12-14 19:06:03 +0100

    try edges

--- src/advent_door_02/NumPadGraph.java
+++ src/advent_door_02/NumPadGraph.java
@@ -4,29 +4,10 @@ import goods.Directions;
 
 public class NumPadGraph 
 {
-	private final static int VERTS = 9;
-	private static int vertsCount = 0;
-	private static Vertex[] vertexList;
-	private static int adjMat[][];
+	
 	
 	public static void start() {
-		vertexList = new Vertex[VERTS];
-		adjMat = new int[VERTS][VERTS];
 		
-		//initialize adjacency matrix
-		for(int j = 0; j < VERTS; j++)
-			for(int k = 0; k < VERTS; k++)
-				adjMat[j][k] = 0;
-		
-		vertexList [vertsCount++] = new Vertex("1");
-		vertexList [vertsCount++] = new Vertex("2");
-		vertexList [vertsCount++] = new Vertex("3");
-		vertexList [vertsCount++] = new Vertex("4");
-		vertexList [vertsCount++] = new Vertex("5");
-		vertexList [vertsCount++] = new Vertex("6");
-		vertexList [vertsCount++] = new Vertex("7");
-		vertexList [vertsCount++] = new Vertex("8");
-		vertexList [vertsCount++] = new Vertex("9");
 	}
 	
 	private static class Vertex
@@ -42,4 +23,34 @@ public class NumPadGraph
 			return name;
 		}
 	}
+	
+	private static class Edges {
+		private char id;
+		//every node has up to 4 edges to an adjacent node
+		private Directions d1 = null;
+		private Directions d2 = null;
+		private Directions d3 = null;
+		private Directions d4 = null;
+		
+		private Edges(char id, boolean north, boolean east, boolean south, boolean west) 
+		{
+			this.id = id;
+			
+			if(north)
+				d1 = Directions.NORTH;
+			if(east)
+				d1 = Directions.EAST;
+			if(south)
+				d1 = Directions.SOUTH;
+			if(west)
+				d1 = Directions.WEST;
+		}
+		
+		@Override
+		public String toString() {
+			return Character.toString(id);
+		}
+	}
+	
+	
 }
